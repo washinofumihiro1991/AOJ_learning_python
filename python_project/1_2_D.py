932e0bd7b1540c22193d11f7b7f2a849653e4208
washinofumihiro1991 <f_washino@yahoo.co.jp>
2017-02-27 01:19:26 +0900

    add 1_2_D

---
+++ python_project/1_2_D.py
@@ -0,0 +1,18 @@
+
+W, H, x, y, r = map(int, input().split())
+
+
+if x - r >= 0 and x + r <= W and y - r >= 0 and y + r <= H:
+    print("Yes")
+else:
+    print("No")
+
+# if x - r >= 0 and x + r <= W:
+#     if y - r >= 0 and y + r <= H:
+#         print("Yes")
+#     else:
+#         print("No")
+# else:
+#     print("No")
+
+
